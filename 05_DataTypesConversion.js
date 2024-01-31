77a55dc387ec4c7674dd899871aca6b0c0c5c358
Sachii1113 <158160104+Sachii1113@users.noreply.github.com>
2024-01-31 15:39:53 +0530

    Create 05_DataTypesConversion.js

---
+++ 05_DataTypesConversion.js
@@ -0,0 +1,20 @@
+let i = 29
+let str = String(i)
+//console.log(str);
+//console.log(typeof(str));
+
+let bol = 3
+let bol1 = Boolean(bol)
+//console.log(bol1)
+let num ="21abc"
+let strq = Number(num)
+//console.log(typeof(strq),strq)
+let n = null
+//console.log(typeof n);
+let m ;
+//console.log(typeof m);
+let st = Number(m)
+//console.log(typeof m);
+let isBoole = true
+let freestrin = String(isBoole)
+console.log(freestrin);
