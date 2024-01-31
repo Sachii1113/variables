1716c1031af91508e90498ee4fc1db6d86ef4ad7
Sachii1113 <158160104+Sachii1113@users.noreply.github.com>
2024-01-31 15:46:33 +0530

    Create dates.js

---
+++ 01_basics/01_basics/dates.js
@@ -0,0 +1,11 @@
+// console.log(Date());
+// const myDate =new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+let dob = new Date("1998-03-19 04:30:01:133")
+//console.log(dob.toDateString());
+console.log(dob.toLocaleString());
+console.log(dob);
