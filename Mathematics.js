99e7063f0d4a97f74b52c2a9fc8b9829289e1ff2
Sachii1113 <158160104+Sachii1113@users.noreply.github.com>
2024-01-31 15:43:22 +0530

    Create Mathematics.js

---
+++ Mathematics.js
@@ -0,0 +1,12 @@
+const hundreds = 10000000
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.67));
+// console.log(Math.ceil(3.001));
+// console.log(Math.floor(3.9999999999));
+// console.log(Math.random()* 10);
+// console.log(Math.floor(Math.random()*10));
+console.log("LOG");
+const max =20
+const min = 10
+console.log(Math.floor(Math.random()*(max-min +1))+min);
